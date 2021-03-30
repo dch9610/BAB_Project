f1f55e524b66170c5489022a42e847411808dc0f
kamunbee <luno14@naver.com>
2021-03-30 17:07:50 +0900

    git test

--- Web/blueprint/board_blueprint.py
+++ Web/blueprint/board_blueprint.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from flask import render_template
+from flask import request
 from flask import session
 
 # blueprint 객체 생성
@@ -8,6 +9,7 @@ board_blue = Blueprint('board', __name__)
 # main을 요청하면 호출되는 함수
 @board_blue.route('/board_main')
 def board_main() :
+
     
     html = render_template('boards/board_main.html')
     return html
